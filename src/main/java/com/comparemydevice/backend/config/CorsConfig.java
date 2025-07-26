f9b10877a322cedf75a21b8a97506c1fd8c3cc8c
rupchandkale <kalerupchand@gmail.com>
2025-07-26 16:24:58 +0530

    working code

--- src/main/java/com/comparemydevice/backend/config/CorsConfig.java
+++ src/main/java/com/comparemydevice/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*") // ✅ fixed here
+                        .allowedOrigins("http://localhost:3000", "https://comparemydevice.com")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
