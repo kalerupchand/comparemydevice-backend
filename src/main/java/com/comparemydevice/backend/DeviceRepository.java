b01b44ba833c7324a2c7ef6e7008bb068accfc0b
rupchandkale <kalerupchand@gmail.com>
2025-07-26 11:40:38 +0530

    deleted device repository

--- src/main/java/com/comparemydevice/backend/DeviceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.comparemydevice.backend;
-
-import com.comparemydevice.backend.entity.Device;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeviceRepository extends JpaRepository<Device, Long> {}
